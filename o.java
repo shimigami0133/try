a582b06c89c0d57911183d26a06312eb2fc58071
abhiram reddy <shimigami0133@gmail.com>
2021-06-10 19:04:59 +0530

    adding java file

---
+++ o.java
@@ -0,0 +1,11 @@
+class test {
+    public int add(int i, int j) {
+        return i - j;
+    }
+
+    public static void main(String args[]) {
+        test t1 = new test();
+        System.out.println(t1.add(1, 2));
+    }
+
+}
